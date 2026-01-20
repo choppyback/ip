c43b385e339ad499fb0963b314bc3012725b08bb
choppyback <seth.tay2812@gmail.com>
2026-01-20 10:27:16 +0800

    Rename Duke to Woody and update logo
    Changed the ASCII logo to become Woody.
    
    Renamed the class to Woody, hence file name
    had to be changed

---
+++ src/main/java/Woody.java
@@ -0,0 +1,13 @@
+public class Woody {
+    public static void main(String[] args) {
+        // Used chatgpt to generate the logo
+        String logo =
+                  "W       W   OOOO   OOOO   DDDDD   Y     Y\n"
+                + "W       W  O    O O    O  D    D   Y   Y \n"
+                + "W   W   W  O    O O    O  D     D    Y Y  \n"
+                + " W W W W   O    O O    O  D    D      Y   \n"
+                + "  W   W     OOOO   OOOO   DDDDD       Y   \n";
+
+        System.out.println("Hello from\n" + logo);
+    }
+}
