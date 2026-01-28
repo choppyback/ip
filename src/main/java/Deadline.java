57bb243b349f030cc76676261c1e73634a01796b
choppyback <seth.tay2812@gmail.com>
2026-01-28 21:54:59 +0800

    Update Deadline and Event to use date-time objects

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,26 @@
+import java.time.LocalDateTime;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
 
     public Deadline(String description, String by) {
+        super(description);
+        this.by = LocalDateTime.parse(by, INPUT_FORMAT);
+    }
+
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toFileString() {
-        return String.format("D %s | %s", super.toFileString(), by.trim());
+        return String.format("D %s| %s", super.toFileString(), by);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by.trim() + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
 }
 
