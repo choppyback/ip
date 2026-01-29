ea6d0ab47c5afee88c3c2b7e9c989078b0a5c2bd
choppyback <seth.tay2812@gmail.com>
2026-01-29 23:56:52 +0800

    Introduce UI class for user interaction
    The UI class handles reading user input and printing output.

---
+++ src/main/java/UI.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class UI {
+    private Scanner scanner;
+
+    public UI() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void showLogo() {
+        String logo =
+                  "W       W   OOOO   OOOO   DDDDD   Y     Y\n"
+                + "W       W  O    O O    O  D    D   Y   Y \n"
+                + "W   W   W  O    O O    O  D     D    Y Y  \n"
+                + " W W W W   O    O O    O  D    D      Y   \n"
+                + "  W   W     OOOO   OOOO   DDDDD       Y   \n";
+
+        System.out.println("Hello from\n\n" + logo);
+        showLine();
+    }
+
+    public void showLine() {
+        System.out.println("-----------------------");
+    }
+
+    public void showWelcome() {
+        System.out.println("Hello! I'm Woody");
+        System.out.println("What can I do for you?\n");
+        showLine();
+    }
+
+    public String readCommand() {
+        System.out.print("You: ");
+        return scanner.nextLine().trim();
+    }
+
+    public void showBye() {
+        showLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        showLine();
+    }
+
+    public void showError(String message) {
+        showLine();
+        System.out.println(message);
+        showLine();
+    }
+
+    public void showTaskAdded(Task task, int size) {
+        showLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + size + " tasks in the list.");
+        showLine();
+    }
+
+    public void showTaskRemoved(Task task, int size) {
+        showLine();
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("  " + task);
+        System.out.println("Now you have " + size + " tasks in the list.");
+        showLine();
+    }
+
+    public void showTaskMarked(Task task) {
+        showLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + task);
+        showLine();
+    }
+
+    public void showTaskUnmarked(Task task) {
+        showLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  " + task);
+        showLine();
+    }
+
+    public void showTaskList(TaskList tasks) {
+        showLine();
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+        showLine();
+    }
+}
