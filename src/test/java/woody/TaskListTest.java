928355b62c698ec086fa6e00580232ac8d0f48c4
choppyback <seth.tay2812@gmail.com>
2026-01-30 16:59:35 +0800

    Add JUnit test for TaskList add method

---
+++ src/test/java/woody/TaskListTest.java
@@ -0,0 +1,17 @@
+package woody;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import woody.task.TaskList;
+import woody.task.ToDo;
+
+public class TaskListTest {
+    @Test
+    public void add_oneTask_sizeIncreases() {
+        TaskList tasks = new TaskList();
+        tasks.add(new ToDo("read book"));
+
+        assertEquals(1, tasks.size(), "TaskList size should increase to 1 after adding one task");
+    }
+}
