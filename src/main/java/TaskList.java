476def009a548ee187387311b4f1836a26b28d3f
choppyback <seth.tay2812@gmail.com>
2026-01-30 15:01:17 +0800

    Rename fields for naming convention
    Change the field list to tasks and method getList to getTasks

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,33 +1,33 @@
 import java.util.ArrayList;
 
 public class TaskList {
-    private ArrayList<Task> list;
+    private ArrayList<Task> tasks;
 
     public TaskList() {
-        list = new ArrayList<>();
+        tasks = new ArrayList<>();
     }
 
-    public TaskList(ArrayList<Task> list) {
-        this.list = list;
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
     }
 
     public void add(Task task) {
-        list.add(task);
+        tasks.add(task);
     }
 
     public Task get(int index) {
-        return list.get(index);
+        return tasks.get(index);
     }
 
     public Task remove(int index) {
-        return list.remove(index);
+        return tasks.remove(index);
     }
 
     public int size() {
-        return list.size();
+        return tasks.size();
     }
 
-    public ArrayList<Task> getList() {
-        return list;
+    public ArrayList<Task> getTasks() {
+        return tasks;
     }
 }
