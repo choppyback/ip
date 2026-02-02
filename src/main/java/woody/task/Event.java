54e6b8e2695c0cba9fdca70a73de871dbbfe329d
choppyback <seth.tay2812@gmail.com>
2026-02-02 09:29:26 +0800

    Add JavaDoc comments to Event

--- src/main/java/woody/task/Event.java
+++ src/main/java/woody/task/Event.java
@@ -1,16 +1,33 @@
 package woody.task;
 import java.time.LocalDateTime;
 
+/**
+ * Represents an event task with a description and a start and end time.
+ */
 public class Event extends Task {
     protected LocalDateTime from;
     protected LocalDateTime to;
 
+    /**
+     * Constructs an event task using date-time strings.
+     *
+     * @param description Description of the event.
+     * @param from Start date and time of the event.
+     * @param to End date and time of the event.
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = LocalDateTime.parse(from.trim(), INPUT_FORMAT);
         this.to = LocalDateTime.parse(to.trim(), INPUT_FORMAT);
     }
 
+    /**
+     * Constructs an event task using date-time objects.
+     *
+     * @param description Description of the event.
+     * @param from Start date and time of the event.
+     * @param to End date and time of the event.
+     */
     public Event(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
