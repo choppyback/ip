26b6610ea499aeb1497e963f00f3ed8cd1c6739f
choppyback <seth.tay2812@gmail.com>
2026-02-02 09:32:35 +0800

    Add JavaDoc comments to Deadline

--- src/main/java/woody/task/Deadline.java
+++ src/main/java/woody/task/Deadline.java
@@ -1,14 +1,29 @@
 package woody.task;
 import java.time.LocalDateTime;
 
+/**
+ * Represents a deadline task with a description and a due date.
+ */
 public class Deadline extends Task {
     protected LocalDateTime by;
 
+    /**
+     * Constructs a deadline task using date-time string.
+     *
+     * @param description Description of the task.
+     * @param by End date and time of the task.
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = LocalDateTime.parse(by, INPUT_FORMAT);
     }
 
+    /**
+     * Constructs a deadline task using date-time object.
+     *
+     * @param description Description of the task.
+     * @param by End date and time of the task.
+     */
     public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
