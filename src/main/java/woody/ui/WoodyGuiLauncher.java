e0c60379449e344973372f0aeca43f900f8675f8
choppyback <seth.tay2812@gmail.com>
2026-02-04 19:27:45 +0800

    Add GUI launcher workaround

--- src/main/java/woody/ui/WoodyGuiLauncher.java
+++ src/main/java/woody/ui/WoodyGuiLauncher.java
@@ -5,8 +5,8 @@ import javafx.application.Application;
 /**
  * A launcher class to workaround classpath issues.
  */
-public class Launcher {
+public class WoodyGuiLauncher {
     public static void main(String[] args) {
-        Application.launch(MainApp.class, args);
+        Application.launch(WoodyGui.class, args);
     }
 }
